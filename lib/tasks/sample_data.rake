1bb6b5aad5b98c982a029a3dc0a7b2661c609fa5
p-tricky <phloftus@gmail.com>
2014-07-02 02:04:12 -0700

    Add user microposts

--- lib/tasks/sample_data.rake
+++ lib/tasks/sample_data.rake
@@ -10,7 +10,7 @@ namespace :db do
                  email: "dodds@harveymudd.edu",
                  password: "ilovespam",
                  password_confirmation: "ilovespam")
-    99.times do |n|
+    20.times do |n|
       name = Faker::Name.name
       email = "example-#{n+1}@railstutorial.org"
       password = "password"
@@ -18,6 +18,11 @@ namespace :db do
                    email: email,
                    password: password,
                    password_confirmation: password)
+    end
+    users = User.all(limit: 6)
+    10.times do
+      content = Faker::Lorem.sentence(5)
+      users.each { |user| user.microposts.create!(content: content) }
     end
   end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -49,7 +49,20 @@ describe "Authentication" do
     describe "for non-signed in users" do
       let(:user) { FactoryGirl.create(:user) }
 
-      describe "when attempting to vise a protected page" do
+      describe "in the Microposts controller" do
+
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
+
+      describe "when attempting to view a protected page" do
         before do
           visit edit_user_path(user)
           fill_in "Email", with: user.email
