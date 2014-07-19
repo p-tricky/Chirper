f3af3fdbb068909fdb5dbe7ee3251d0e07d0a9c9
p-tricky <phloftus@gmail.com>
2014-07-18 17:22:47 -0700

    users bug

--- lib/tasks/sample_data.rake
+++ lib/tasks/sample_data.rake
@@ -1,6 +1,12 @@
 namespace :db do
   desc "Fill database with sample data"
   task populate: :environment do
+    make_users
+    make_microposts
+    make_relationships
+  end
+end
+def make_users
     admin = User.create!(name: "Boss man",
                  email: "Boss@hardknocks.edu",
                  password: "bigdick",
@@ -10,7 +16,7 @@ namespace :db do
                  email: "dodds@harveymudd.edu",
                  password: "ilovespam",
                  password_confirmation: "ilovespam")
-    20.times do |n|
+    98.times do |n|
       name = Faker::Name.name
       email = "example-#{n+1}@railstutorial.org"
       password = "password"
@@ -19,10 +25,21 @@ namespace :db do
                    password: password,
                    password_confirmation: password)
     end
+end
+
+def make_microposts
     users = User.all(limit: 6)
-    10.times do
+    50.times do
       content = Faker::Lorem.sentence(5)
       users.each { |user| user.microposts.create!(content: content) }
     end
-  end
+end
+
+def make_relationships
+  users = User.all
+  user = users.first
+  followed_users = users[2..50]
+  followers = users[3..40]
+  followed_users.each { |followed| user.follow!(followed) }
+  followers.each { |follower| follower.follow!(user) }
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -49,6 +49,21 @@ describe "Authentication" do
     describe "for non-signed in users" do
       let(:user) { FactoryGirl.create(:user) }
 
+      describe "when attempting to view a protected page" do
+        before do
+          visit edit_user_path(user)
+          fill_in "Email", with: user.email
+          fill_in "Password", with: user.password
+          click_button "Sign in"
+        end
+
+        describe "after sign in" do
+          it "should render the desired protected page" do
+            expect(page).to have_title('Edit user')
+          end
+        end
+      end
+
       describe "in the Microposts controller" do
 
         describe "submitting to the create action" do
@@ -62,18 +77,15 @@ describe "Authentication" do
         end
       end
 
-      describe "when attempting to view a protected page" do
-        before do
-          visit edit_user_path(user)
-          fill_in "Email", with: user.email
-          fill_in "Password", with: user.password
-          click_button "Sign in"
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { expect(response).to redirect_to(signin_path) }
         end
 
-        describe "after sign in" do
-          it "should render the desired protected page" do
-            expect(page).to have_title('Edit user')
-          end
+        describe "submitting to the destroy action" do
+          before { delete relationship_path }
+          specify { expect(response).to redirect_to(signin_path) }
         end
       end
 
@@ -91,6 +103,16 @@ describe "Authentication" do
 
         describe "visiting the user index" do
           before { visit users_path }
+          it { should have_title('Sign in') }
+        end
+
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
           it { should have_title('Sign in') }
         end
       end
